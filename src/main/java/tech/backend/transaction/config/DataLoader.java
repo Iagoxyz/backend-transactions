f40736c713d719e30bc200139223592ed2b2e35a
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-02-28 16:24:00 -0300

    Database seeding

--- src/main/java/tech/backend/transaction/config/DataLoader.java
+++ src/main/java/tech/backend/transaction/config/DataLoader.java
@@ -2,12 +2,23 @@ package tech.backend.transaction.config;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import tech.backend.transaction.entity.WalletType;
+import tech.backend.transaction.repository.WalletTypeRepository;
+
+import java.util.Arrays;
 
 @Configuration
-public class DataLoader extends CommandLineRunner {
+public class DataLoader implements CommandLineRunner {
+
+    private final WalletTypeRepository walletTypeRepository;
+
+    public DataLoader(WalletTypeRepository walletTypeRepository) {
+        this.walletTypeRepository = walletTypeRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-
+         Arrays.stream(WalletType.Enum.values())
+                 .forEach(walletType -> walletTypeRepository.save(walletType.get()));
     }
 }

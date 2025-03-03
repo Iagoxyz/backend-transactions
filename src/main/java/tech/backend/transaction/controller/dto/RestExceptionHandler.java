9882c4c40fe6924227f978f1676655ea9f2dad89
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-03-03 09:44:32 -0300

    Validation error response

--- src/main/java/tech/backend/transaction/controller/dto/RestExceptionHandler.java
+++ src/main/java/tech/backend/transaction/controller/dto/RestExceptionHandler.java
@@ -1,10 +1,14 @@
 package tech.backend.transaction.controller.dto;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tech.backend.transaction.exception.TransactionException;
+
+import java.util.List;
+
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -13,4 +17,21 @@ public class RestExceptionHandler {
     public ProblemDetail handleTransactionException(TransactionException e) {
         return e.toProblemDetail();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        var fieldErrors = e.getFieldErrors()
+                .stream()
+                .map(f -> new InvalidParam(f.getField(), f.getDefaultMessage()))
+                .toList();
+
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pb.setDetail("Your request parameters didn't validate");
+        pb.setProperty("invalid-params", fieldErrors);
+
+        return pb;
+    }
+
+    private record InvalidParam(String name, String reason) {}
 }
